7c980af5a4e42b92d3100b7b0041ef08d9a329c6
khoi.le <khoi.le@advahealthsolutions.com>
2022-04-06 14:57:19 +0700

    init components

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+
+const Input = () => {
+  return (
+    <>
+      <Text>Input Component</Text>
+    </>
+  );
+};
+
+export default Input;
